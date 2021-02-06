0a5f309e7acc2417db3dd256a41ee1b2891e6ade
Axel Andersson <axel.andersson86@gmail.com>
2021-02-06 19:36:33 +0100

    Fix icon line up in mobile view and login dummy on desktop

--- client/src/components/Navbar/Header.tsx
+++ client/src/components/Navbar/Header.tsx
@@ -3,6 +3,7 @@ import styles from "./Header.module.scss";
 const Header = () => (
   <div className={styles.header}>
     <input type="text" placeholder="Search ingredients" />
+    <p>Sign up</p>
     <i className="far fa-user"></i>
   </div>
 );
