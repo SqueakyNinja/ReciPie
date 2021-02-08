3e6dd209803c525d2e4991c019988d5cecb3e353
Axel Andersson <axel.andersson86@gmail.com>
2021-02-08 17:13:07 +0100

    Add Signup page

---
+++ client/src/pages/Signup/FormSuccess.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormSuccess = () => {
+    return (
+       <div className="form-content-right">
+           <div className="form-success">You have registered</div>
+       </div>
+    )
+}
+
+export default FormSuccess
