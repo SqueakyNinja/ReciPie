f5750bc74ad2e9fe70941c7e52abeca8db516669
angiescript <johansson.angelika@hotmail.com>
2021-02-18 15:16:02 +0100

    add primary and secondary colors

--- client/src/pages/Style/index.tsx
+++ client/src/pages/Style/index.tsx
@@ -4,14 +4,14 @@ import styles from "./index.module.scss";
 const Style = () => {
   return (
     <div>
+      <Button variant="contained" className={styles.secondaryButton}>
+        Button
+      </Button>
       <Button variant="contained" className={styles.button}>
         Button
       </Button>
-      <Button variant="contained" color="secondary" className={styles.button}>
-        Button
-      </Button>
       <button className={styles.button}>Button</button>
-      <TextField></TextField>
+      <TextField variant="outlined"></TextField>
     </div>
   );
 };
