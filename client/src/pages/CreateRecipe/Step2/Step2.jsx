147af7c954bfdcd16de81cc493edc3cf30228db2
Anna Blankell <anna.blankell@outlook.com>
2021-02-22 11:16:10 +0100

    Update options Step2.

--- client/src/pages/CreateRecipe/Step2/Step2.jsx
+++ client/src/pages/CreateRecipe/Step2/Step2.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
 
+import {
+  Button,
+  TextField,
+  FormControl,
+  Select,
+  ListSubheader,
+  InputLabel,
+  MenuItem,
+} from '@material-ui/core';
+
 const Step2 = () => {
   return (
     <div className='Step2'>
-      <label htmlFor='search-ingredients'>Add ingredients: </label>
+      <TextField variant='outlined' label='Search Ingredients'></TextField>
       <br />
-      <input
-        type='text'
-        name='search-ingredients'
-        placeholder='Enter ingredient'
-      />
       <br />
-      <div className='Step2-measurement'>
-        <input type='text' />
-        <select name='estimated-time'>
-          <optgroup label='Imperial'>
-            <option value='tsp'>teaspoon (tsp)</option>
-            <option value='tbsp'>tablespoon (tbsp)</option>
-            <option value='cup'>cup (c)</option>
-            <option value='oz'>ounce (oz)</option>
-            <option value='pt'>pint (pt)</option>
-            <option value='qt'>quart (qt)</option>
-            <option value='gal'>gallon (gal)</option>
-            <option value='lb'>pound (lb)</option>
-          </optgroup>
-          <optgroup label='Metric'>
-            <option value='mtsp'>teaspoon (tsp)</option>
-            <option value='mtbsp'>tablespoon (tbsp)</option>
-            <option value='ml'>milliliter (ml)</option>
-            <option value='cl'>centiliter (cl)</option>
-            <option value='dl'>deciliter (dl)</option>
-            <option value='l'>liter (l)</option>
-            <option value='g'>grams (g)</option>
-            <option value='kg'>kilograms (kg)</option>
-          </optgroup>
-        </select>
+      <div className='Step2Measurement'>
+        <TextField variant='outlined' label='Measurements' type='number' />
+
+        <FormControl variant='outlined'>
+          <InputLabel>Grouping</InputLabel>
+          {/* <Select native defaultValue=''> */}
+          <Select label='Groupinhg'>
+            {/* <option aria-label='None' value='' /> */}
+            <optgroup label='Imperial'>
+              <option value='tsp'>teaspoon (tsp)</option>
+              <option value='tbsp'>tablespoon (tbsp)</option>
+              <option value='cup'>cup (c)</option>
+              <option value='oz'>ounce (oz)</option>
+              <option value='pt'>pint (pt)</option>
+              <option value='qt'>quart (qt)</option>
+              <option value='gal'>gallon (gal)</option>
+              <option value='lb'>pound (lb)</option>
+            </optgroup>
+            <optgroup label='Metric'>
+              <option value='mtsp'>teaspoon (tsp)</option>
+              <option value='mtbsp'>tablespoon (tbsp)</option>
+              <option value='ml'>milliliter (ml)</option>
+              <option value='cl'>centiliter (cl)</option>
+              <option value='dl'>deciliter (dl)</option>
+              <option value='l'>liter (l)</option>
+              <option value='g'>grams (g)</option>
+              <option value='kg'>kilograms (kg)</option>
+            </optgroup>
+          </Select>
+        </FormControl>
       </div>
     </div>
   );
