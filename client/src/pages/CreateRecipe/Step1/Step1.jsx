d06ce29de813cfbe248cf7f79780309a12d757c7
Anna Blankell <anna.blankell@outlook.com>
2021-02-22 09:46:19 +0100

    Add material ui inputs to Step 1

---
+++ client/src/pages/CreateRecipe/Step1/Step1.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import {
+  Button,
+  TextField,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+} from '@material-ui/core';
+
+const Step1 = () => {
+  return (
+    <div>
+      <TextField variant='outlined' label='Name of Recipe'></TextField>
+
+      <br />
+      <br />
+      <TextField
+        variant='outlined'
+        label='Number of portions'
+        type='number'
+        // InputLabelProps={{
+        //   shrink: true,
+        // }}
+      />
+      {/* <input
+        type='number'
+        name='no-portions'
+        placeholder='Enter number of portions'
+      /> */}
+      <br />
+      <br />
+
+      <label htmlFor='estimated-time'>Estimated time</label>
+      <div>
+        <FormControl variant='outlined'>
+          <InputLabel>Hours</InputLabel>
+          <Select labelId='demo-simple-select-outlined-label' label='Hours'>
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value='0'>0</MenuItem>
+            <MenuItem value='1'>1</MenuItem>
+            <MenuItem value='2'>2</MenuItem>
+            <MenuItem value='3'>3</MenuItem>
+            <MenuItem value='4'>4</MenuItem>
+            <MenuItem value='5'>5</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant='outlined'>
+          <InputLabel>Minutes</InputLabel>
+          <Select label='Minutes'>
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value='0'>0</MenuItem>
+            <MenuItem value='5'>5</MenuItem>
+            <MenuItem value='10'>10</MenuItem>
+            <MenuItem value='15'>15</MenuItem>
+            <MenuItem value='20'>20</MenuItem>
+            <MenuItem value='25'>25</MenuItem>
+            <MenuItem value='30'>30</MenuItem>
+            <MenuItem value='35'>35</MenuItem>
+            <MenuItem value='40'>40</MenuItem>
+            <MenuItem value='45'>45</MenuItem>
+            <MenuItem value='50'>50</MenuItem>
+            <MenuItem value='55'>55</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default Step1;
