3a9387814a3abea69b0a1e74d8f9a19e8ce802f5
Axel Andersson <axel.andersson86@gmail.com>
2021-02-23 11:34:41 +0100

    Add paper to formsuccess.

--- client/src/pages/Account/FormSuccess.jsx
+++ client/src/pages/Account/FormSuccess.jsx
@@ -1,18 +1,20 @@
 import styles from "./index.module.scss";
-import Signup from "./Signup";
+import { Button, TextField, Paper } from "@material-ui/core";
 
 const FormSuccess = ({ values }) => {
   return (
     <div className={styles.signupRight}>
-      <div className={styles.formSuccess}>
-        <h1>Thank you {values.username} for register!</h1>
-        <img
-          className={styles.formImg2}
-          src={"/images/thumbsUp.png"}
-          alt="success"
-        />
-        <p>Click here to go to the login page</p>
-      </div>
+      <Paper className={styles.formPaper}>
+        <div className={styles.form}>
+          <h1>Thank you {values.username} for register!</h1>
+          <img
+            className={styles.formImg2}
+            src={"/images/thumbsUp.png"}
+            alt="success"
+          />
+          <p>Click here to go to the login page</p>
+        </div>
+      </Paper>
     </div>
   );
 };
