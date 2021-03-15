536536cb2817929803b030aa11a98f4b52993d36
Anna Blankell <anna.blankell@outlook.com>
2021-03-15 12:29:09 +0100

    Change style.

--- client/src/pages/MealGenerator/RecipeDetails.jsx
+++ client/src/pages/MealGenerator/RecipeDetails.jsx
@@ -1,4 +1,4 @@
-import styles from "./MealCard.module.scss";
+import styles from './MealCard.module.scss';
 
 const RecipeDetails = ({ recipe }) => {
   return recipe.extendedIngredients ? (
@@ -8,11 +8,13 @@ const RecipeDetails = ({ recipe }) => {
       <div className={styles.mealInfo}>
         <h2>{recipe.title}</h2>
         {recipe.sourceName && <p>{recipe.sourceName}</p>}
-        <i className="far fa-heart"></i>
-        <i className="fas fa-heart"></i>
+        <i className='far fa-heart'></i>
+        <i className='fas fa-heart'></i>
         {recipe.servings > 0 && <p>Servings: {recipe.servings}</p>}
         {recipe.aggregateLikes && <p>{recipe.aggregateLikes} likes</p>}
-        {recipe.readyInMinutes > 0 && <p>Prep time: {recipe.readyInMinutes} min</p>}
+        {recipe.readyInMinutes > 0 && (
+          <p>Prep time: {recipe.readyInMinutes} min</p>
+        )}
       </div>
 
       <div className={styles.ingredients}>
@@ -23,8 +25,9 @@ const RecipeDetails = ({ recipe }) => {
               <li key={index}>
                 {i.measures.metric.amount > 0 && (
                   <span>
-                    {i.measures.metric.amount}
-                    {i.measures.metric.unitShort}
+                    {`
+                   ${i.measures.metric.amount} 
+                    ${i.measures.metric.unitShort} `}
                   </span>
                 )}
                 {i.name.length > 0 && <span>{i.name}</span>}
