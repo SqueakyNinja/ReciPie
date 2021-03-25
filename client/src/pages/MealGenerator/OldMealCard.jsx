fe35f054754feee774404cca5098916105e820d0
Anna Blankell <anna.blankell@outlook.com>
2021-03-25 16:00:34 +0100

    Style MealCard.

---
+++ client/src/pages/MealGenerator/OldMealCard.jsx
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import RecipeDetails from '../../components/RecipeDetails/RecipeDetails';
+
+const OldMealCard = () => {
+  const recipeId = useParams().id;
+  const apiKey = '8080ada856dd4f439b4a065ae353d836';
+
+  const [recipe, setRecipe] = useState({});
+
+  const fetchRecipe = async () => {
+    const result = await axios(
+      `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`
+    );
+    setRecipe(result.data);
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <RecipeDetails recipe={recipe} />
+    </div>
+  );
+};
+
+export default OldMealCard;
