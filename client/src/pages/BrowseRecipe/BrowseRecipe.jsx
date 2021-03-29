670f746ad195f03b2d876ea3ac0470e22ae8c566
angiescript <johansson.angelika@hotmail.com>
2021-03-29 15:18:47 +0200

    add FilterData file to populate accordions.

--- client/src/pages/BrowseRecipe/BrowseRecipe.jsx
+++ client/src/pages/BrowseRecipe/BrowseRecipe.jsx
@@ -3,20 +3,21 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import { FixedSizeList } from 'react-window';
+import { FixedSizeList } from "react-window";
 import { Paper } from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import Search from "../MealGenerator/Search";
+import { CuisineData, DietData, TypeData } from "./FilterData";
 import styles from "./BrowseRecipe.module.scss";
 import axios from "axios";
 import MealGrid from "../MealGenerator/MealGrid";
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const BrowseRecipe = () => {
   const [checked, setChecked] = useState([0]);
 
@@ -35,17 +36,26 @@ const BrowseRecipe = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [query, setQuery] = useState("");
+  const [type, setType] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [diet, setDiet] = useState("");
 
   const fetchRecipes = async () => {
-    const result = await axios("https://api.spoonacular.com/recipes/complexSearch", {
-      params: {
-        apiKey: "34a95b9efbbe41dbaa0ba4b9d0d76287",
-        // apiKey: "8080ada856dd4f439b4a065ae353d836",
-        query: query,
-        number: 2,
-        sort: "popularity",
-      },
-    });
+    const result = await axios(
+      "https://api.spoonacular.com/recipes/complexSearch",
+      {
+        params: {
+          apiKey: "34a95b9efbbe41dbaa0ba4b9d0d76287",
+          // apiKey: "8080ada856dd4f439b4a065ae353d836",
+          query: query,
+          number: 2,
+          sort: "popularity",
+          type: type,
+          cuisine: cuisine,
+          diet: diet,
+        },
+      }
+    );
     setRecipes(result.data.results);
     /*const savedRecipes = await getAllRecipes("", false, query);
     const savedApiRecipes = savedRecipes.recipes
@@ -72,88 +82,142 @@ const BrowseRecipe = () => {
   return (
     <div className={styles.browseContainer}>
       <div className={styles.searchBox}>
-        <Search query={query} setQuery={setQuery}/>
-        <button onClick={ () => console.log(recipes)}>klicka</button>
+        <Search query={query} setQuery={setQuery} />
+        <button onClick={() => console.log(recipes)}>klicka</button>
       </div>
       <div className={styles.contentContainer}>
-       <div className={styles.filters}>
-       <Paper className={styles.filterPaper}>
-          <h1>Filter your search</h1>
-        <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Type</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          <List>
-        {[0, 1, 2, 3].map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
+        <div className={styles.filters}>
+          <Paper className={styles.filterPaper}>
+            <h1>Filter your search</h1>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Type</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <List>
+                    {TypeData.map((value) => {
+                      const labelId = `checkbox-list-label-${value.value}`;
 
-          return (
-            <ListItem
-              key={value}
-              role={undefined}
-              dense
-              button
-              onClick={handleToggle(value)}
-              className={styles.eachListItems}
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-             className={styles.eachListItemsCheckbox}
+                      return (
+                        <ListItem
+                          key={value.value}
+                          role={undefined}
+                          dense
+                          button
+                          onClick={handleToggle(value)}
+                          className={styles.eachListItems}
+                        >
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={checked.indexOf(value) !== -1}
+                              tabIndex={-1}
+                              disableRipple
+                              inputProps={{ "aria-labelledby": labelId }}
+                              className={styles.eachListItemsCheckbox}
+                            />
+                          </ListItemIcon>
+                          <ListItemText id={labelId} primary={value.title} />
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Cuisine</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <List>
+                    {CuisineData.map((value) => {
+                      const labelId = `checkbox-list-label-${value.value}`;
 
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItem>
-          );
-        })}
-      </List>
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Cuisine</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Något mer</Typography>
-        </AccordionSummary>
-      </Accordion>
-       
-    
-      </Paper> </div>
-        <div className={styles.searchResults}><MealGrid recipes={recipes}  /></div>
+                      return (
+                        <ListItem
+                          key={value.value}
+                          role={undefined}
+                          dense
+                          button
+                          onClick={handleToggle(value)}
+                          className={styles.eachListItems}
+                        >
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={checked.indexOf(value) !== -1}
+                              tabIndex={-1}
+                              disableRipple
+                              inputProps={{ "aria-labelledby": labelId }}
+                              className={styles.eachListItemsCheckbox}
+                            />
+                          </ListItemIcon>
+                          <ListItemText id={labelId} primary={value.title} />
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2a-content"
+                id="panel2a-header"
+              >
+                <Typography>Diet</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <List>
+                    {DietData.map((value) => {
+                      const labelId = `checkbox-list-label-${value.value}`;
 
-
+                      return (
+                        <ListItem
+                          key={value}
+                          role={undefined}
+                          dense
+                          button
+                          onClick={handleToggle(value)}
+                          className={styles.eachListItems}
+                        >
+                          <ListItemIcon>
+                            <Checkbox
+                              edge="start"
+                              checked={checked.indexOf(value) !== -1}
+                              tabIndex={-1}
+                              disableRipple
+                              inputProps={{ "aria-labelledby": labelId }}
+                              className={styles.eachListItemsCheckbox}
+                            />
+                          </ListItemIcon>
+                          <ListItemText id={labelId} primary={value.title} />
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </Paper>{" "}
+        </div>
+        <div className={styles.searchResults}>
+          <MealGrid recipes={recipes} />
+        </div>
       </div>
-      
-      
     </div>
   );
 };
--- client/src/pages/BrowseRecipe/FilterData.jsx
+++ client/src/pages/BrowseRecipe/FilterData.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+export const CuisineData = [
+  {
+    value: "italian",
+    title: "Italian",
+  },
 
-function FilterData() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  {
+    value: "japanese",
+    title: "Japanese",
+  },
 
-export default FilterData
+  {
+    value: "french",
+    title: "French",
+  },
+
+  {
+    value: "asian",
+    title: "Asian",
+  },
+
+  {
+    value: "mediterranean",
+    title: "Mediterranean",
+  },
+];
+
+export const DietData = [
+  {
+    value: "ketogenic",
+    title: "Ketogenic",
+  },
+
+  {
+    value: "vegetarian",
+    title: "Vegetarian",
+  },
+
+  {
+    value: "paleo",
+    title: "Paleo",
+  },
+
+  {
+    value: "gluten+free",
+    title: "Gluten Free",
+  },
+
+  {
+    value: "vegan",
+    title: "Vegan",
+  },
+];
+
+export const TypeData = [
+  {
+    value: "main+course",
+    title: "Main Course",
+  },
+
+  {
+    value: "dessert",
+    title: "Dessert",
+  },
+
+  {
+    value: "appetizer",
+    title: "Appetizer",
+  },
+
+  {
+    value: "breakfast",
+    title: "Breakfast",
+  },
+
+  {
+    value: "snack",
+    title: "Snack",
+  },
+  
+];
