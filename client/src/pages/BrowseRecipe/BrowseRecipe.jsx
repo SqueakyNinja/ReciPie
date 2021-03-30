5f92719e2c7fc3943aece7d45c5ca16b00609864
Anna Blankell <anna.blankell@outlook.com>
2021-03-30 13:49:48 +0200

    Add remove checkbox.

--- client/src/pages/BrowseRecipe/BrowseRecipe.jsx
+++ client/src/pages/BrowseRecipe/BrowseRecipe.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import { FixedSizeList } from "react-window";
-import { Paper } from "@material-ui/core";
-import ListItemText from "@material-ui/core/ListItemText";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import Search from "../MealGenerator/Search";
-import { CuisineData, DietData, TypeData } from "./FilterData";
-import styles from "./BrowseRecipe.module.scss";
-import axios from "axios";
-import MealGrid from "../MealGenerator/MealGrid";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { FixedSizeList } from 'react-window';
+import { Paper } from '@material-ui/core';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Search from '../MealGenerator/Search';
+import { CuisineData, DietData, TypeData } from './FilterData';
+import styles from './BrowseRecipe.module.scss';
+import axios from 'axios';
+import MealGrid from '../MealGenerator/MealGrid';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const BrowseRecipe = () => {
   const [checked, setChecked] = useState([]);
 
   const [recipes, setRecipes] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [type, setType] = useState([]);
   const [cuisine, setCuisine] = useState([]);
   const [diet, setDiet] = useState([]);
@@ -35,37 +35,48 @@ const BrowseRecipe = () => {
       newChecked.push(value);
 
       switch (value.param) {
-        case "type":
-
-          console.log([...type, value.value])
+        case 'type':
+          console.log([...type, value.value]);
           setType([...type, value.value]);
           break;
 
-        case "cuisine":
+        case 'cuisine':
           setCuisine([...cuisine, value.value]);
           break;
 
-        case "diet":
+        case 'diet':
           setDiet([...diet, value.value]);
           break;
 
         default:
           break;
       }
-      
     } else {
       newChecked.splice(currentIndex, 1);
 
       switch (value.param) {
-        case "type":
+        case 'type':
+          const newTypes = type.filter((x) => x != value.value);
+          //console.log(type);
+          // console.log(value.value);
+          // console.log(newTypes);
+          setType(newTypes);
+
+          // const removeProduct = useCallback(
+          //   (id) => {
+          //     setProducts(products.filter((product) => product.id != id));
+          //   },
+          //   [products]
+          // );
+
           //setType(""); Här vill vi ta bort endast det value som skickas in som parameter
           break;
 
-        case "cuisine":
+        case 'cuisine':
           //setCuisine(""); Här vill vi ta bort endast det value som skickas in som parameter
           break;
 
-        case "diet":
+        case 'diet':
           //setDiet(""); Här vill vi ta bort endast det value som skickas in som parameter
           break;
       }
@@ -80,21 +91,21 @@ const BrowseRecipe = () => {
     const cuisineString = cuisine.join(',');
 
     const result = await axios(
-      "https://api.spoonacular.com/recipes/complexSearch",
+      'https://api.spoonacular.com/recipes/complexSearch',
       {
         params: {
-          apiKey: "34a95b9efbbe41dbaa0ba4b9d0d76287",
+          apiKey: '34a95b9efbbe41dbaa0ba4b9d0d76287',
           // apiKey: "8080ada856dd4f439b4a065ae353d836",
           query: query,
           number: 2,
-          sort: "popularity",
+          sort: 'popularity',
           type: typeString,
           diet: dietString,
           cuisine: cuisineString,
         },
       }
     );
-console.log(result.data.totalResults)
+    console.log(result.data.totalResults);
     setRecipes(result.data.results);
     /*const savedRecipes = await getAllRecipes("", false, query);
     const savedApiRecipes = savedRecipes.recipes
@@ -118,6 +129,7 @@ console.log(result.data.totalResults)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query, checked]);
+
   return (
     <div className={styles.browseContainer}>
       <div className={styles.searchBox}>
@@ -131,8 +143,8 @@ console.log(result.data.totalResults)
             <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
                 <Typography>Type</Typography>
               </AccordionSummary>
@@ -152,11 +164,11 @@ console.log(result.data.totalResults)
                         >
                           <ListItemIcon>
                             <Checkbox
-                              edge="start"
+                              edge='start'
                               checked={checked.indexOf(value) !== -1}
                               tabIndex={-1}
                               disableRipple
-                              inputProps={{ "aria-labelledby": labelId }}
+                              inputProps={{ 'aria-labelledby': labelId }}
                               className={styles.eachListItemsCheckbox}
                             />
                           </ListItemIcon>
@@ -165,14 +177,14 @@ console.log(result.data.totalResults)
                       );
                     })}
                   </List>
-                  </div>
+                </div>
               </AccordionDetails>
             </Accordion>
             <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
+                aria-controls='panel2a-content'
+                id='panel2a-header'
               >
                 <Typography>Cuisine</Typography>
               </AccordionSummary>
@@ -193,11 +205,11 @@ console.log(result.data.totalResults)
                         >
                           <ListItemIcon>
                             <Checkbox
-                              edge="start"
+                              edge='start'
                               checked={checked.indexOf(value) !== -1}
                               tabIndex={-1}
                               disableRipple
-                              inputProps={{ "aria-labelledby": labelId }}
+                              inputProps={{ 'aria-labelledby': labelId }}
                               className={styles.eachListItemsCheckbox}
                             />
                           </ListItemIcon>
@@ -212,8 +224,8 @@ console.log(result.data.totalResults)
             <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
+                aria-controls='panel2a-content'
+                id='panel2a-header'
               >
                 <Typography>Diet</Typography>
               </AccordionSummary>
@@ -234,11 +246,11 @@ console.log(result.data.totalResults)
                         >
                           <ListItemIcon>
                             <Checkbox
-                              edge="start"
+                              edge='start'
                               checked={checked.indexOf(value) !== -1}
                               tabIndex={-1}
                               disableRipple
-                              inputProps={{ "aria-labelledby": labelId }}
+                              inputProps={{ 'aria-labelledby': labelId }}
                               className={styles.eachListItemsCheckbox}
                             />
                           </ListItemIcon>
@@ -250,7 +262,7 @@ console.log(result.data.totalResults)
                 </div>
               </AccordionDetails>
             </Accordion>
-          </Paper>{" "}
+          </Paper>{' '}
         </div>
         <div className={styles.searchResults}>
           <MealGrid recipes={recipes} />
